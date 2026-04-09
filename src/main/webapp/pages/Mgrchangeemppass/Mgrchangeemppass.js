bdf9a4414d786a246f6fbd4b618f80085c7b4a34
Nirosha Golla <nirosha.golla@wavemaker.com>
2026-04-09 11:35:28 +0000

    Integrated api for password

--- src/main/webapp/pages/Mgrchangeemppass/Mgrchangeemppass.js
+++ src/main/webapp/pages/Mgrchangeemppass/Mgrchangeemppass.js
@@ -14,6 +14,50 @@ Page.onReady = function () {
      * e.g. to get value of text widget named 'username' use following script
      * 'Page.Widgets.username.datavalue'
      */
+    
+    // Initialize password strength bar to empty state
+    Page.Widgets.passwordStrengthBar.datavalue = 0;
+    Page.Widgets.passwordStrengthBar.type = 'danger';
+    Page.Widgets.passwordStrengthLabel.caption = 'Password Strength';
+    Page.Widgets.passwordStrengthLabel.class = 'password-strength-label strength-empty';
+};
+
+Page.checkPasswordStrength = function($event, widget, newVal, oldVal) {
+    var password = newVal || '';
+    var strengthBar = Page.Widgets.passwordStrengthBar;
+    var strengthLabel = Page.Widgets.passwordStrengthLabel;
+    
+    // Check password strength based on criteria
+    var hasUpperCase = /[A-Z]/.test(password);
+    var hasDigit = /\d/.test(password);
+    var isLongEnough = password.length >= 8;
+    
+    // Update strength bar and label based on password criteria
+    if (password.length === 0) {
+        // Empty password - red
+        strengthLabel.caption = 'Password Strength';
+        strengthLabel.class = 'password-strength-label strength-empty';
+        strengthBar.datavalue = 0;
+        strengthBar.type = 'danger';
+    } else if (password.length < 8) {
+        // Too Short - red
+        strengthLabel.caption = 'Too Short';
+        strengthLabel.class = 'password-strength-label strength-weak';
+        strengthBar.datavalue = 33;
+        strengthBar.type = 'danger';
+    } else if (isLongEnough && (!hasUpperCase || !hasDigit)) {
+        // Medium - orange/warning
+        strengthLabel.caption = 'Medium';
+        strengthLabel.class = 'password-strength-label strength-medium';
+        strengthBar.datavalue = 66;
+        strengthBar.type = 'warning';
+    } else if (isLongEnough && hasUpperCase && hasDigit) {
+        // Strong - green
+        strengthLabel.caption = 'Strong';
+        strengthLabel.class = 'password-strength-label strength-strong';
+        strengthBar.datavalue = 100;
+        strengthBar.type = 'success';
+    }
 };
 
 Page.svMgrUpdateEmpPwdonSuccess = function (variable, data) {
