d3ab2b0fb31faa876c78fb8fb648738f5264906d
Jothilakshmi Marimuthu <jothi.marimuthu@wavemaker.com>
2026-04-15 13:41:37 +0000

    Position view - UI changes

---
+++ services/getPositionViewScheduling/src/com/wavemaker/when_to_work/getPositionViewScheduling/service/GetPositionViewSchedulingService.java
@@ -0,0 +1,59 @@
+package com.wavemaker.when_to_work.getPositionViewScheduling.service;
+
+
+import com.wavemaker.when_to_work.getPositionViewScheduling.model.*;
+import com.wavemaker.when_to_work.getPositionViewScheduling.model.RootResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.lang.Object;
+import org.springframework.util.MultiValueMap;
+import feign.*;
+
+public interface GetPositionViewSchedulingService {
+
+  /**
+   * 
+   * 
+    * @param Authorization Authorization (optional)
+    * @param companyId companyId (optional)
+    * @param endDate endDate (optional)
+    * @param startDate startDate (optional)
+   * @return RootResponse
+   */
+  @RequestLine("GET /scheduling/shifts/date-position?companyId={companyId}&endDate={endDate}&startDate={startDate}")
+  @Headers({
+    "Accept: application/json",
+    "Authorization: {Authorization}"  })
+  RootResponse invoke(@Param("Authorization") String Authorization, @Param("companyId") String companyId, @Param("endDate") String endDate, @Param("startDate") String startDate);
+
+
+    /**
+     * 
+     * 
+     * Note, this is equivalent to the other <code>invoke</code> method,
+     * but with the query parameters collected into a single Map parameter. This
+     * is convenient for services with optional query parameters, especially when
+     * used with the {@link InvokeQueryParams} class that allows for
+     * building up this map in a fluent style.
+     * @param Authorization Authorization (optional)
+     * @param queryParams Map of query parameters as name-value pairs
+     *   <p>The following elements may be specified in the query map:</p>
+     *   <ul>
+     *   <li>companyId - companyId (optional)</li>
+     *   <li>endDate - endDate (optional)</li>
+     *   <li>startDate - startDate (optional)</li>
+     *   </ul>
+     * @return RootResponse
+     */
+    @RequestLine("GET /scheduling/shifts/date-position?companyId={companyId}&endDate={endDate}&startDate={startDate}")
+    @Headers({
+    "Accept: application/json",
+        "Authorization: {Authorization}"    })
+    RootResponse invoke
+    (@Param("Authorization") String Authorization, @QueryMap(encoded=true)
+    MultiValueMap<String, String> queryParams);
+
+}
